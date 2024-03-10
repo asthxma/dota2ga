a86ec667be019e196850b2307525ad9aecbf52fc
Mochammad Itmamul Wafa <47962260+Mamul-11@users.noreply.github.com>
2024-03-10 08:04:42 +0700

    update_dataset
    `

--- pages/2_Dataset.py
+++ pages/2_Dataset.py
@@ -1,5 +1,25 @@
+import streamlit as st
 import pandas as pd
+from PIL import Image
+from streamlit_extras.switch_page_button import switch_page 
 
+<<<<<<< Updated upstream
 df = pd.read_csv('dota2ga/dataset/dota2_dataset.csv')
 print(df)
 
+=======
+st.set_page_config(
+    page_title="Dataset Dota 2",
+    page_icon="👋",
+    layout="wide",
+)
+
+st.sidebar.success("Silahkan memilih laman yang ingin dituju.")
+
+st.write("# Dataset Dota 2")
+    
+st.subheader('Berikut merupakan penjabaran dari dataset dota 2 yang dimana menampilkan 124 hero', divider='rainbow')
+
+df = pd.read_csv('F:\\applications\\Python\\Magister\\KKPM\\dota2ga\\dataset\\dota2_heroes.csv')
+st.write(df)
+>>>>>>> Stashed changes
