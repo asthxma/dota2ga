a35b5372d4a4d46fefd4a7f34237b49fa3ad6a2c
muhammadsalam15 <muhammadsalam.business@gmail.com>
2024-03-17 10:35:25 +0700

    Update 4_test.py

--- pages/4_test.py
+++ pages/4_test.py
@@ -10,7 +10,7 @@ st.set_page_config(
     page_icon=im,
     layout="wide",
 )
-# st.sidebar.success("Silahkan memilih laman yang ingin dituju.")
+# st.sidebar.success("Silaahkan memilih laman yang ingin dituju.")
 st.write("# Test")
 
 
