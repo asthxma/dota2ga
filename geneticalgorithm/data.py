bb426d68326c8ac3bbd0f6de50dc2d2622542980
asthxma <86945935+asthxma@users.noreply.github.com>
2024-03-21 14:00:29 +0700

    adding genetic algorithm
    adding genetic algorithm

--- geneticalgorithm/data.py
+++ geneticalgorithm/data.py
@@ -9,8 +9,8 @@ def preprocess_data(file_path):
     df['complexity'] = pd.to_numeric(df['complexity'])
     df['win_rate'] = pd.to_numeric(df['win_rate'])
     df['pick_rate'] = pd.to_numeric(df['pick_rate'])
-    df['Balanced']= ((df['total_base_attribute']+ df['movement_speed'])/df['complexity']) * ((df['win_rate']+ df['pick_rate']) / 2)
-    hero_df = df[['hero', 'primary_role', 'Balanced']]
+    df['fitness']= ((df['total_base_attribute']+ df['movement_speed'])/df['complexity']) * ((df['win_rate']+ df['pick_rate']) / 2)
+    hero_df = df[['hero', 'primary_role','fight_role', 'fitness']]
 
     safe = df[df['primary_role'] == 'safelane']
     mid = df[df['primary_role'] == 'midlane']
@@ -21,19 +21,15 @@ def preprocess_data(file_path):
     # Create a list of dataframes for each role
     roles = [safe, mid, off, softsupp, hardsupp]
 
-    # Calculate the sum of balanced team composition for each role
-    max_balanced_sums = [role.groupby('primary_role').apply(lambda x: (x['Balanced']).max()).sum() for role in roles]
+    # Calculate the sum of fitness team composition for each role
+    max_fitness_sums = [role.groupby('primary_role').apply(lambda x: (x['fitness']).max()).sum() for role in roles]
 
     # Create a DataFrame to display the results
     result_df = pd.DataFrame({
         'Role': ['safelane', 'midlane', 'offlane', 'soft support', 'hard support'],
-        'max_balanced_sums': max_balanced_sums
+        'max_fitness_sums': max_fitness_sums
     })
 
-    # Calculate the total sum of max balanced team composition
-    total_balance = result_df['max_balanced_sums'].sum()
-    return df, hero_df, total_balance
-
-#file_path = 'dataset\dota2_heroes.csv'
-
-#df, hero_df, total_balance = preprocess_data(file_path)
+    # Calculate the total sum of max fitness team composition
+    total_fitness = result_df['max_fitness_sums'].sum()
+    return df, hero_df, total_fitness
