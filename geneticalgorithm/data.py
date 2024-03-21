564f2ef472bc5668426444b37116e54507368788
asthxma <86945935+asthxma@users.noreply.github.com>
2024-03-21 10:45:46 +0700

    finalize preprocess data
    finalize preprocess data

--- geneticalgorithm/data.py
+++ geneticalgorithm/data.py
@@ -1,15 +1,9 @@
 import pandas as pd
-#from nama_modul import df
-
 
 def preprocess_data(file_path):
     df = pd.read_csv(file_path)
     df['win_rate'] = df['win_rate'].str.replace('%', '')
     df['pick_rate'] = df['pick_rate'].str.replace('%', '')
-    # df['Hero_Defense'] = df['Mag_Defence'] + df['Phy_Defence']
-    # df['Win_Rate'] = df['Esport_Wins'] / (df['Esport_Wins'] + df['Esport_Loss'])
-    # df['Hard_Engage'] = (df['Phy_Damage'] + df['Mov_Speed'])
-    # df['Team_Fight'] = (df['Phy_Damage'] + df['Hp'] + df['Hero_Defense'])
     df['total_base_attribute'] = pd.to_numeric(df['total_base_attribute'])
     df['movement_speed'] = pd.to_numeric(df['movement_speed'])
     df['complexity'] = pd.to_numeric(df['complexity'])
@@ -17,7 +11,7 @@ def preprocess_data(file_path):
     df['pick_rate'] = pd.to_numeric(df['pick_rate'])
     df['Balanced']= ((df['total_base_attribute']+ df['movement_speed'])/df['complexity']) * ((df['win_rate']+ df['pick_rate']) / 2)
     hero_df = df[['hero', 'primary_role', 'Balanced']]
-#primary
+
     safe = df[df['primary_role'] == 'safelane']
     mid = df[df['primary_role'] == 'midlane']
     off = df[df['primary_role'] == 'offlane']
@@ -25,30 +19,21 @@ def preprocess_data(file_path):
     hardsupp = df[df['primary_role'] == 'hard support']
 
     # Create a list of dataframes for each role
-    roles = [safe, mid, off, softsupp,hardsupp]
+    roles = [safe, mid, off, softsupp, hardsupp]
 
-    # Calculate the sum of (Hard_Engage * Win_Rate) and (Team_Fight * Win_Rate) for each role
-    # max_hard_engage_sums = [role.groupby('Lane').apply(lambda x: (x['Hard_Engage'] * x['Win_Rate']).max()).sum() for role in roles]
-    # max_team_fight_sums = [role.groupby('Lane').apply(lambda x: (x['Team_Fight'] * x['Win_Rate']).max()).sum() for role in roles]
+    # Calculate the sum of balanced team composition for each role
     max_balanced_sums = [role.groupby('primary_role').apply(lambda x: (x['Balanced']).max()).sum() for role in roles]
+
     # Create a DataFrame to display the results
     result_df = pd.DataFrame({
         'Role': ['safelane', 'midlane', 'offlane', 'soft support', 'hard support'],
-        'max_balanced_sums': max_balanced_sums,
-        # 'Max_Team_Fight_Sum': max_team_fight_sums
+        'max_balanced_sums': max_balanced_sums
     })
 
-    # Calculate the total sum of (Hard_Engage * Win_Rate) and (Team_Fight * Win_Rate)
+    # Calculate the total sum of max balanced team composition
     total_balance = result_df['max_balanced_sums'].sum()
-    # total_tf = result_df['Max_Team_Fight_Sum'].sum()
     return df, hero_df, total_balance
 
-file_path = 'dataset\dota2_heroes.csv'
+#file_path = 'dataset\dota2_heroes.csv'
 
-df, hero_df, total_balance = preprocess_data(file_path)
-
-print(df,"\n")
-
-print(hero_df,"\n")
-
-print(total_balance)
+#df, hero_df, total_balance = preprocess_data(file_path)
