437d7fcfbfe9bd028996b7452dff9448357442ac
Hasda12 <143325859+Hasda12@users.noreply.github.com>
2024-03-23 12:09:37 +0700

    4

--- pages/3_Team_Composition.py
+++ pages/3_Team_Composition.py
@@ -3,6 +3,11 @@ import streamlit as st
 from PIL import Image
 from streamlit_extras.switch_page_button import switch_page 
 from streamlit.components.v1 import html
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from geneticalgorithm.Data import preprocess_data
+from geneticalgorithm.GeneticAlgorithm import genetic_algorithm
 
 im = Image.open("iconsdota.png")
 st.set_page_config(
@@ -11,5 +16,44 @@ st.set_page_config(
     layout="wide",
 )
 
+
+file_path = 'dataset\dota2_heroes.csv'
+df, hero_df, total_fit= preprocess_data(file_path)
+hero_data = hero_df.to_dict(orient='records')
 st.write("# Team Composition Dota 2")
 
+
+# Set algorithm parameters
+target_positions = st.multiselect(
+    'Input Target Lineup',
+    ['safelane', 'midlane', 'offlane', 'soft support', 'hard support'],
+    ['safelane', 'midlane', 'offlane', 'soft support', 'hard support']
+    )
+
+
+# Initialize input
+pop_size = st.number_input('Population Numbers', min_value=1, value=10)
+population = [random.sample(hero_data[:-1], 5) for _ in range(pop_size)]
+
+generations = st.number_input('generations', min_value=1, value=30)
+tournament_size = st.number_input('Tournament Size', min_value=1, value=3)
+crossover_rate = st.number_input('Crossover Rate', value=0.8, max_value=1.00)
+mutation_rate = st.number_input('Mutation Rate', value=0.10, max_value=1.00)
+
+# Run genetic algorithm for hard engage
+result, temp_result = genetic_algorithm(population, hero_data, target_positions, generations, tournament_size, crossover_rate, mutation_rate, pop_size)
+
+chart_data = pd.DataFrame({
+    "Generations": list(range(1, len(temp_result)+1)),
+    "Fitness": (temp_result / total_fit) * 100
+    #"Team Fight": (temp_result_team_fight / total_tf) * 100
+})
+
+st.subheader('Fitness Value')
+st.line_chart(chart_data, x='Generations', y="Fitness", color="#dc143c", width=0, height=0, use_container_width=False)
+
+st.subheader("Team Composition:")
+result_df = pd.DataFrame.from_dict(result)
+st.dataframe(result_df.drop(columns='fitness'))
+fitness_tf = ((temp_result[-1] / total_fit) * 100)
+st.write(f"Fitness: {fitness_tf:.2f}%")
